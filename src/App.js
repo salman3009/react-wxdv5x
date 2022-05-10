5a07f238101c07c535ad214dbb78bd0ac9f3ef77
salman3009 <salman444g@gmail.com>
2022-05-10 11:05:14 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+   let fullName = "salman";
+   let age = 56;
+   let flag = true;
+
+   const addition = (a,b)=>{
+     return a+b;
+   }
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+       <p>{fullName}</p>
+       <p>{age}</p>
+       <p>{flag}</p>
+       <p>{flag?"true":"false"}</p>
+       <p>{2+4}</p>
+       <p>{"hi welcome to "+fullName}</p>
+       <p>{addition(3,4)}</p>
     </div>
   );
 }
